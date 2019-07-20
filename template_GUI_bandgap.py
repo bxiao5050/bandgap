73275a3593efede49b4f9ac4d9afcfd0e817d670
= <bxiao5050@gmail.com>
2019-07-20 15:42:53 +0200

    import built-in modules

--- template_GUI_bandgap.py
+++ template_GUI_bandgap.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 import glob
-import os
+# import os
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from tkinter import *
 import matplotlib
